1371f0966c9075384ee539d5eb228aa76fc6ab4a
wenqingxu615 <516770005@qq.com>
2021-03-29 19:06:40 -0400

    search pages

---
+++ wbdv-sp01-project/src/services/book-service.js
@@ -0,0 +1,12 @@
+export const findBookByTitle = (title) =>
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
+    .then(response => response.json())
+
+export const findBookById = (id) =>
+    fetch(`https://www.googleapis.com/books/v1/volumes/${id}`)
+    .then(response => response.json())
+
+export default {
+    findBookByTitle,
+    findBookById
+}
